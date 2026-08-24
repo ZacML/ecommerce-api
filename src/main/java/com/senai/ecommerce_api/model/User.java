034a4c555bd6e4f93c1e91767b1b321d49458728
ZacML <isaquem673@gmail.com>
2026-08-24 20:23:32 -0300

    Primeiro Commit

---
+++ src/main/java/com/senai/ecommerce_api/model/User.java
@@ -0,0 +1,39 @@
+package com.senai.ecommerce_api.model;
+
+import com.senai.ecommerce_api.dto.UserDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nome;
+    @NotNull
+    private String cpf;
+    @NotNull
+    private String endereco;
+    @NotNull
+    private String telefone;
+    @NotNull
+    private LocalDate data_cadastro;
+
+    public static User convert(UserDTO userDTO) {
+        User user = new User();
+        user.setNome(userDTO.getNome());
+        user.setCpf(userDTO.getCpf());
+        user.setEndereco(userDTO.getEndereco());
+        user.setTelefone(userDTO.getTelefone());
+        user.setData_cadastro(userDTO.getDataCadastro());
+
+        return user;
+    }
+}
