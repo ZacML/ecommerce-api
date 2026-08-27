89a46d82bf8928a11ebe9ae60658f5e76d5ff0cf
ZacML <isaquem673@gmail.com>
2026-08-27 13:25:45 -0300

    ajustes

--- src/main/java/com/senai/ecommerce_api/controller/UserController.java
+++ src/main/java/com/senai/ecommerce_api/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.senai.ecommerce_api.controller;
 
 import com.senai.ecommerce_api.dto.UserDTO;
+import com.senai.ecommerce_api.service.UserService;
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,59 +14,42 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/")
-    public String getMensagem() {
-        return "Spring boot está em modo operante Mestre!";
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDTO> getUsers() {
+        return userService.getAll();
     }
 
-    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
-
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setNome("Eduardo");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua a");
-        userDTO.setEmail("eduardo@email.com");
-        userDTO.setTelefone("1234-3454");
-        userDTO.setDataCadastro(LocalDateTime.now());
-
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setNome("Luiz");
-        userDTO2.setCpf("456");
-        userDTO2.setEndereco("Rua b");
-        userDTO2.setEmail("luiz@email.com");
-        userDTO2.setTelefone("1234-3454");
-        userDTO2.setDataCadastro(LocalDateTime.now());
-
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setNome("Bruna");
-        userDTO3.setCpf("678");
-        userDTO3.setEndereco("Rua c");
-        userDTO3.setEmail("bruna@email.com");
-        userDTO3.setTelefone("1234-3454");
-        userDTO3.setDataCadastro(LocalDateTime.now());
-
-        usuarios.add(userDTO);
-        usuarios.add(userDTO2);
-        usuarios.add(userDTO3);
-    }
-
-    @GetMapping("/{cpf}")
-    public UserDTO getUsersFiltro(@PathVariable String cpf) {
-        return usuarios .stream()
-                .filter(userDTO -> userDTO.getCpf().equals(cpf))
-                .findFirst() .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    @GetMapping("/{id}")
+    public UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO inserir(@RequestBody UserDTO userDTO) {
-        userDTO.setDataCadastro(LocalDateTime.now());
-        usuarios.add(userDTO);
-        return userDTO;
+    public UserDTO newUser(@RequestBody @Valid UserDTO userDTO) {
+        return userService.save(userDTO);
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<UserDTO> queryByName(
+            @RequestParam(name = "nome", required = true) String nome) {
+        return userService.queryByName(nome);
     }
 
 }
